2aae75980396df0fb3db2dce370be3bda257c5e8
Besukal gobena <besukalgobena@gmail.com>
2026-02-25 15:43:47 +0300

    creating book store api with crud operation

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -7,7 +7,7 @@ const getAllBooks = async (req, res) => {
     if (allBooks?.length > 0) {
       res.status(200).json({
         succcess: true,
-        message: "Here are our books",
+        message: "Here are your books",
         data: allBooks,
       });
     } else {
@@ -24,7 +24,32 @@ const getAllBooks = async (req, res) => {
     });
   }
 };
-const getSingleBook = async (req, res) => {};
+
+const getSingleBook = async (req, res) => {
+  try {
+    const getCurrentBookId = req.params.id;
+    const singleBook = await Book.findById(getCurrentBookId);
+    if (!singleBook) {
+      return res.status(404).json({
+        succcess: false,
+        message: "Book is not found",
+      });
+    }
+
+    res.status(200).json({
+      succcess: true,
+      message: "Here is the book with detail information",
+      data: singleBook,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      succcess: false,
+      message: "Something went wrong on server!",
+    });
+  }
+};
+
 const addNewBook = async (req, res) => {
   try {
     const newBookFormData = req.body;
@@ -41,8 +66,65 @@ const addNewBook = async (req, res) => {
     console.log(error);
   }
 };
-const updateBook = async (req, res) => {};
-const deleteBook = async (req, res) => {};
+
+const updateBook = async (req, res) => {
+  try {
+    const updateBookDataForm = req.body;
+    const currentBookId = req.params.id;
+    const updateBookById = await Book.findByIdAndUpdate(
+      currentBookId,
+      updateBookDataForm,
+      {
+        new: true,
+      },
+    );
+
+    if (!updateBookById) {
+      return res.status(404).json({
+        succcess: false,
+        message: "Book is not found",
+      });
+    }
+
+    res.status(200).json({
+      succcess: true,
+      message: "Book is updated succesfully",
+      data: updateBookById,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      succcess: false,
+      message: "Something went wrong on server!",
+    });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const currentBook = req.params.id;
+    const deleteBookById = await Book.findByIdAndDelete(currentBook);
+
+    if (!deleteBookById) {
+      return res.status(404).json({
+        succcess: false,
+        message: "Book is not found",
+      });
+    }
+
+    res.status(200).json({
+      succcess: true,
+      message: "Book is deleted succesfully",
+      data: deleteBookById,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      succcess: false,
+      message: "Something went wrong on server!",
+    });
+  }
+};
 
 module.exports = {
   getAllBooks,
